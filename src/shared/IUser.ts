3352518e00a9fa0ca2b7bfeb7cf5b5fd3340f4b2
Michael Robertson <mrobertson@nationalmerchants.org>
2017-08-08 10:54:54 -0700

    updating

--- src/shared/IUser.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file IUser.ts - User interface
- * @author Michael Robertson
- * @version 0.0.1
- */
-export interface IUser {
-  id: number;
-  name: string;
-}
--- test/domain/repo/base/mockType.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @file mockType.ts - mock interface used to test base repository
- * @author Michael Robertson
- * @version 0.0.1
- */
-export interface IMockType {
-  id: number;
-}
