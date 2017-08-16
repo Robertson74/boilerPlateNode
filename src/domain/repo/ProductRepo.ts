f1343e9e03e190e88c1108cc6b14beb20dab3963
Michael Robertson <mrobertson@nationalmerchants.org>
2017-08-16 16:45:13 -0700

    updating

--- src/domain/repo/ProductRepo.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file ProductRepo.ts - repository layer for product model
- * @author Michael Robertson
- * @version 0.0.1
- */
-import { IProduct } from "../../shared/models/IProduct";
-import { BaseRepository } from "./baseInterface/baseRepo";
-export class ProductRepo extends BaseRepository<IProduct> {
-}
