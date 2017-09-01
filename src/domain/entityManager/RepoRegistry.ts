f8da82c7df8f68162fdc5024538222cfc4324e63
Michael Robertson <mrobertson@nationalmerchants.org>
2017-09-01 13:50:44 -0700

    updating

--- src/domain/entityManager/RepoRegistry.ts
+++ src/domain/entityManager/RepoRegistry.ts
@@ -3,30 +3,17 @@
  * @author Michael Robertson
  * @version 0.0.1
  */
-// DO NOT REMOVE OR MOVE THE ### COMMENTS THEY ARE NEEDED FOR THE AUTO REPO GEN
 
 import { BaseBusiness } from "../business/baseInterface/BaseBusiness";
-import { BaseRepository } from "../repo/baseInterface/baseRepo";
 import { UserBusiness } from "../business/UserBusiness";
-import { UserRepo } from "../repo/UserRepo";
-import { MIDRepo } from "../repo/MIDRepo";
-import { MIDBusiness } from "../business/MIDBusiness";
-import { TransactionRepo } from "../repo/TransactionRepo";
-import { TransactionBusiness } from "../business/TransactionBusiness";
-import { NewObjRepo } from "../repo/NewObjRepo";
-import { NewObjBusiness } from "../business/NewObjBusiness"; // ###endRegistryImports
+import { BaseRepository } from "../repo/baseInterface/baseRepo";
+import { UserRepo } from "../repo/UserRepo"; // ###endRegistryImports
 
 // format of registry entries:
 // Model: [ RepoClass, BusinessClass]
 export const repoRegistry: {} = {
-  User: [ UserRepo, UserBusiness ],
-  MID: [ MIDRepo, MIDBusiness ],
-  Transaction: [ TransactionRepo, TransactionBusiness ],
-  NewObj: [ NewObjRepo, NewObjBusiness ] // ###endRepoRegistry
+  User: [ UserRepo, UserBusiness ] // ###endRepoRegistry
 };
 
 export type repoNames =
-  "User" |
-  "MID" |
-  "Transaction" |
-  "NewObj"; // ###endRepoNames
+  "User"; // ###endRepoNames
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,28 +5,3 @@
  */
 /* tslint:disable */
 console.log("sandbox start");
-
-const testing: string = "text";
-
-import "reflect-metadata";
-import { Connection } from "typeorm/connection/Connection";
-import { DevConfig } from "./config/devConfig";
-import { EntityManager } from "./domain/entityManager/EntityManager";
-import { MysqlConnectionCreator } from "./domain/DAL/mysql/connection";
-import { createConnection } from "typeorm";
-import { repoRegistry } from "./domain/entityManager/RepoRegistry";
-import { MID } from "./shared/entities/MID";
-import { MIDBusiness } from "./domain/business/MIDBusiness";
-import { Transaction } from "./shared/entities/Transaction";
-import { TransactionBusiness } from "./domain/business/TransactionBusiness";
-import { NewObj } from "./shared/entities/NewObj";
-import { NewObjBusiness } from "./domain/business/NewObjBusiness";
-
-(async () => {
-  const conn: Connection = await (new MysqlConnectionCreator(new DevConfig(), createConnection)).createMysqlConnection();
-  let em = new EntityManager(conn, repoRegistry);
-  const obj: NewObj = new NewObj();
-  obj.id = 10000;
-  const objRepo: NewObjBusiness = em.getRepository("NewObj");
-
-})();
